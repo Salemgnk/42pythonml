336673bfae07f48933e5c50cdc9807fe5fea7dba
Salemgnk <salem.gnandi@epitech.eu>
2024-07-31 10:28:57 +0100

    Coding the first function

--- module00/ex06/recipe.py
+++ module00/ex06/recipe.py
@@ -1,4 +1,23 @@
 cookbook = {
-    'ingredients': "['rice', 'meat', 'pork', 'ginger']",
-    'meal': "['
+    "sandwich" : {
+        "ingredients" : ['ham', 'bread', 'tomatoes'],
+        'meal':'lunch',
+        'prep_time' : 10
+    },
+    "cake" : {
+        "ingredients" : ['flour', 'sugar', 'eggs'],
+        "meal" : 'dessert',
+        'prep_time' : 60
+    },
+    "salad" : {
+        "ingredients" : ['avocado', 'arugula', 'tomatoes', 'spinach'],
+        "meal" : 'lunch',
+        "prep_time" : 15
+    }
 }
+
+def print_all_recipe_name():
+    for name in cookbook.keys():
+        print(name)
+
+print_all_recipe_name()
